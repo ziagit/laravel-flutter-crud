f311d142f65416547b3bf9992969d3f98d779077
zia <zia.csco@gmail.com>
2020-11-08 21:30:27 -0800

    init

---
+++ flutter/lib/pages/posts.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Posts extends StatefulWidget {
+  @override
+  _PostsState createState() => _PostsState();
+}
+
+class _PostsState extends State<Posts> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text("hi i'm fine"),
+    ));
+  }
+}
